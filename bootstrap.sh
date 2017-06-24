39727cb28b78bfc4ea04822bbff26febc9015e97
isobelsv <isobelascott@gmail.com>
2017-06-24 11:26:18 -0400

    changing bootstap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 apt-get update --fix-missing
-apt-get install vim git zlib1g-dev --assume-yes
+apt-get install vim wget git zlib1g-dev --assume-yes
 
 cd /home/vagrant/
 sudo wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
