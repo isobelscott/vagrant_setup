531d311d5c427a44d5e97651e500a7842279e7fd
isobelsv <isobelascott@gmail.com>
2017-06-29 16:54:09 -0400

    adding get_fall_time to exercise.py

--- assignment_5/exercise.py
+++ assignment_5/exercise.py
@@ -6,4 +6,8 @@ def to_celsius(degrees_fahrenheit):
     degrees_fahrenheit = (degrees_fahrenheit - 32) / 1.8
     return degrees_fahrenheit
 
-   
+import math
+def get_fall_time(height):
+    time_elapsed = math.sqrt((2 * height) / 9.8)
+    return time_elapsed
+
