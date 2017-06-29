e90fdb260ac86170d75d8b91397092dd68dadb2a
isobelsv <isobelascott@gmail.com>
2017-06-29 17:37:46 -0400

    adding isVulnerable function to exercise.py file

--- assignment_5/exercise.py
+++ assignment_5/exercise.py
@@ -11,3 +11,20 @@ def get_fall_time(height):
     time_elapsed = math.sqrt((2 * height) / 9.8)
     return time_elapsed
 
+def isVulnerable(tower_height, tower_x, tower_y, target_x, target_y):
+    muzzle_velocity = 300
+    time_in_air = muzzle_velocity * get_fall_time(tower_height)
+    tower_range = time_in_air * muzzle_velocity
+    
+    delta_x = tower_x - target_x   # difference between tower_x and target_x
+    delta_y = tower_y - target_y # difference between tower_y and target_y
+
+    separation = math.sqrt(delta_x ** 2 + delta_y ** 2)
+    # the x and y deltas form a triangle, find the hypotenuse
+    if separation < tower_range:
+        print("The target is closer than the tower range, what should we return?")
+        return True
+    else:
+        print("The target is further than the tower range, what should we return?")
+        return False
+
