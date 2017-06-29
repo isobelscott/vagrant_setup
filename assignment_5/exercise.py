4527226b465ace725a15d0d7685bd403d47f344c
isobelsv <isobelascott@gmail.com>
2017-06-29 16:26:43 -0400

    adding exercise.py file

---
+++ assignment_5/exercise.py
@@ -0,0 +1,9 @@
+def to_fahrenheit(degrees_celsius):
+    degrees_celsius = degrees_celsius * 1.8 + 32
+    return degrees_celsius
+
+def to_celsius(degrees_fahrenheit):
+    degrees_fahrenheit = (degrees_fahrenheit - 32) / 1.8
+    return degrees_fahrenheit
+
+   
