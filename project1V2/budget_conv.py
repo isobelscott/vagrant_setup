b9fdefad77a7745a6f72b3347480eab3b1593e82
isobelsv <isobelascott@gmail.com>
2017-07-02 16:35:02 -0400

    Currency error handling in.

--- project1V2/budget_conv.py
+++ project1V2/budget_conv.py
@@ -25,10 +25,23 @@ def savebudget(budgetfloat):
 if __name__ == "__main__":
     get_currency = input("What currency are you using? ") 
     get_amount = float(input("How much does it cost? "))
-    get_amount_usd = converter.foreign_currency_to_usd(get_amount, get_currency)
-    old_budget = getrest('budget_database.txt')
-    new_budget = float(old_budget) - get_amount
-    new_budget_fc = converter.usd_to_foreign_currency(new_budget, get_currency)
-    savebudget(new_budget)
-    print("You spent " + str(round(get_amount, 3)) + " " +  str(get_currency) + ". Which is $" + str(get_amount_usd)  + " USD.")
-    print("Your remaining budget is $" + str(new_budget) + " USD. Which is " + str(new_budget_fc) + " " + str(get_currency) + ".")
+    
+    while True:
+        try:
+            get_amount_usd = converter.foreign_currency_to_usd(get_amount, get_currency)
+        except KeyError:
+            print("That's not a supported currency or format. Please enter supported currency in three-letter format.") 
+            break
+        except ValueError:
+            print("That's not a number.")
+            break
+        
+        old_budget = getrest('budget_database.txt')
+        new_budget = float(old_budget) - get_amount
+        new_budget_fc = converter.usd_to_foreign_currency(new_budget, get_currency)
+        savebudget(new_budget)
+        print("You spent " + str(round(get_amount, 2)) + " " +  str(get_currency) + ".")
+        print("Which is $" + str(get_amount_usd)  + " USD.")
+        print("Your remaining budget is $" + str(new_budget) + " USD.")
+        print("Which is " + str(new_budget_fc) + " " + str(get_currency) + ".")
+        break
