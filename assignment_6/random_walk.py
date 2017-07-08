7d7ec2279b771be0fa6f901012a809ae90cb87c0
isobelsv <isobelascott@gmail.com>
2017-07-08 16:36:50 +0000

    adding assignment_6 with optional

--- assignment_6/random_walk.py
+++ assignment_6/random_walk.py
@@ -5,83 +5,96 @@ import sys
 import math
 
 def get_random_direction():
-        direction = ""
-        probability = random.random()
+    direction = ""
+    probability = random.random()
 
-        if probability < 0.25:
-            direction = "west"
-        elif probability < 0.5:
-            direction = "east"
-        elif probability < 0.75: 
-            direction = "north"
-        else: 
-            direction = "south"
+    if probability < 0.25:
+        direction = "west"
+    elif probability < 0.5:
+        direction = "east"
+    elif probability < 0.75:
+        direction = "north" 
+    elif probability < 1: 
+        direction = "south"
 
-        return direction
+    return direction
 
 def get_direction_displacement(dir_key):
-        displacements = {
-                'west': (-1, 0),
-                'east': (1, 0),
-                'north': (0, 1),
-                'south': (0, -1)
-                }
+    displacements = {
+        'west': (-1, 0),
+        'east': (1, 0),
+        'north': (0, 1),
+        'south': (0, -1)
+        }
 
-        return displacements[dir_key]
+    return displacements[dir_key]
 
 def take_walk(steps):
-        current_location = [0, 0]
-        
-        for step_index in range(steps):
-            direction = get_random_direction()
-            displacement = get_direction_displacement(direction)
+    current_location = [0, 0]
+    for step_index in range(steps):
+        direction = get_random_direction()
 
-            delta_x = displacement[0]
-            delta_y = displacement[1]
+        displacement = get_direction_displacement(direction)
 
-            current_location = [0, 0]
-            current_location[0] += delta_x
-            current_location[1] += delta_y
+        # extract the numerical values from the tuple
+        delta_x = displacement[0]
+        delta_y = displacement[1]
 
-        return current_location
+    current_location = [delta_x, delta_y]
+    return current_location 
 
-def take_all_walks(steps):
+def take_all_walks(steps, runs):
     endpoints = []
-    for walks in range(steps):
+    for run_index in range(runs):
         end_location = take_walk(steps)
         endpoints.append(end_location)
     return endpoints
 
-
-def average_final_distance(endpoints):
-    total_distance = 0
+def average_distance(endpoints):
+    total_distance = 0 
     for coords in endpoints:
         dx = coords[0]
         dy = coords[1]
 
-        # use the Pythagorean theorem to get distance like last session
-        distance = math.sqrt( dx ** 2 + dy ** 2)
+        distance = math.sqrt(dx ** 2 + dy ** 2)
 
         total_distance += distance
 
     return total_distance / len(endpoints)
 
-# def average_position(endpoints):
-    
+
+def average_position(current_location):
+    for coords in current_location:
+        dx = coords[0]
+        dy = coords[1]
+    avgdx = dx / len(current_location)
+    avgdy = dy / len(current_location)
+
+    average_coords = [avgdx, avgdy]
+    return average_coords
+
 
 if __name__ == "__main__":
+    
     steps = 10
     if len(sys.argv) > 1:
-    steps = int(sys.argv[1])
-        
-    runs = 10
+         steps = int(sys.argv[1])
+   
+    runs = 3
     if len(sys.argv) > 2:
-    steps = int(sys.argv[2])
+         runs = int(sys.argv[2])
 
-    end_locations  = take_all_walks(runs)
-    average_displacement = average_final_distance(end_locations)
-        
-    print("Done with all walks, printing all end locations: ")
+    end_locations =  take_all_walks(steps, runs)
+    current_location = take_walk(steps)
+
+    print("Done with walks, printing end locations: ")
     print(end_locations)
-    print("The average distance covered on these walks is: ")
+
+    average_displacement = average_distance(end_locations)
+    print("The average distance covered is: ")     
     print(average_displacement)
+    
+    average_coords = average_position(end_locations)
+    print("The average position ended up in: ")
+    print(average_coords)
+
